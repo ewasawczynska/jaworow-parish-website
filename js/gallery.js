38b8afe2b5b70af2319c5c86c23f047fde61480b
ewasawczynska <sawczynska.e@gmail.com>
2024-05-08 23:07:54 +0200

    init settings: gallery

---
+++ js/gallery.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.gallery img');
+    let currentIndex = 0;
+
+    function toggleImages() {
+        images[currentIndex].classList.remove('img-active');
+        currentIndex = (currentIndex + 1) % images.length;
+        images[currentIndex].classList.add('img-active');
+    }
+
+    setInterval(toggleImages, 3000);
+});
+
