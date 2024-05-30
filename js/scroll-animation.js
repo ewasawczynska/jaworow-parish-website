9696e89e6570c4b4c23dc598ea08443f620162be
ewasawczynska <sawczynska.e@gmail.com>
2024-05-30 14:06:00 +0200

    fix-2: scroll animation

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,4 +1,4 @@
-import ScrollReveal from 'scrollreveal';
+import { ScrollReveal } from './scrollreveal.min.js';
 
 window.sr = ScrollReveal();
 sr.reveal('.masses-subheader, .masses-txt, .masses-hours__item, .michael-prayer__title, .michael-prayer__text', {
